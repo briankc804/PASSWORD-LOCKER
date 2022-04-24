a554a59558e3f3aecd362925866658b63c2cc8ca
briankc804 <briankc804@gmail.com>
2022-04-24 13:39:16 +0300

    created a save contact method that is called on a contact object and appends it to our contact-list

--- user.py
+++ user.py
@@ -7,6 +7,11 @@ class User:
 
     def __init__(self,user_name,pass_word):
       #dockstring removed for simplicity
-      
+
          self.user_name = user_name
          self.pass_word = pass_word
+    def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+        User.user_list.append(self)
