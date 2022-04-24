dec83c84da62606c24bd54aa3a28160fc8f97339
briankc804 <briankc804@gmail.com>
2022-04-24 12:28:12 +0300

    created a new instance using init

--- user.py
+++ user.py
@@ -3,6 +3,9 @@ class User:
     Class that generates new instances of users
     '''
 
-    # user_list = [] #Empt user list
+    user_list = [] #Empt user list
 
-    # def __init__(self,user_name)
+    def __init__(self,user_name,pass_word):
+        
+         self.user_name = user_name
+         self.pass_word = pass_word
