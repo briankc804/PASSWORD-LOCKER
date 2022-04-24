67b34bffb99850b2ebd95ddbca8f3412a9d9c46e
briankc804 <briankc804@gmail.com>
2022-04-24 16:01:47 +0300

    added a teardown method that that executes a set instructions after every test

--- user_test.py
+++ user_test.py
@@ -30,7 +30,15 @@ class TestUser(unittest.TestCase):
         test_save_user test case to test if the user user is saved into the user list
         '''  
         self.new_user.save_user() #saving the user
-        self.assertEqual(len(User.user_list),1)     
+        self.assertEqual(len(User.user_list),1)  
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+   
+
     def test_save_multiple_user(self):
         '''
          test_save_multiple_user to check if we can save multiple user
@@ -41,5 +49,6 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
 
+   
 if __name__ == '__main__':
     unittest.main()
