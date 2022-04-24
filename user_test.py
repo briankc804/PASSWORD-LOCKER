812700ccd9c4cf87a363b241e8d5e1dea16e3046
briankc804 <briankc804@gmail.com>
2022-04-24 12:49:24 +0300

    created a subclass  called TestContacts

--- user_test.py
+++ user_test.py
@@ -1,2 +1,10 @@
 import unittest #importing the unittest module
 from user import User #importing the user classs
+
+class TestUser(unittest.TestCase):
+     '''
+     Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+     '''
