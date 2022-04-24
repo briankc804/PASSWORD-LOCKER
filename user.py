3e3ab6771be588b38d2bd60811c425ecd6f5ce62
briankc804 <briankc804@gmail.com>
2022-04-24 23:56:11 +0300

    created a credentials class

--- user.py
+++ user.py
@@ -1,3 +1,7 @@
+
+from re import U
+import pyperclip
+
 class User:
     '''
     Class that generates new instances of users
@@ -10,8 +14,63 @@ class User:
 
          self.user_name = user_name
          self.pass_word = pass_word
+         
+         
+        
+         
     def save_user(self):
         '''
         save_user method saves user objects into user_list
         '''
         User.user_list.append(self)
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)  
+
+    @classmethod
+    def find_by_user_name(cls,user_name):
+            '''
+            Method that takes in a username and returns a user that matches that username.
+
+            Args:
+                username: name to search for
+            Returns :
+                user of person that matches the username.
+            '''
+
+            for user in cls.user_list:
+                if user.user_name == user_name:
+                    return user        
+
+
+    @classmethod
+    def user_exist(cls,user_name):
+            '''
+            Method that checks if a user exists from the user list.
+            Args:
+                user_name: user_name to search if it exists
+            Returns :
+                Boolean: True or false depending if the user exists
+            '''
+            for user in cls.user_list:
+                if user.user_name == user_name:
+                        return True
+
+            return False               
+
+    @classmethod
+    def display_users(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list  
+
+    @classmethod
+    def copy_pass_word(cls,user_name):
+        user_found = User.find_by_user_name(user_name)
+        pyperclip.copy(user_found.pass_word)          
--- user_test.py
+++ user_test.py
@@ -1,6 +1,6 @@
 import unittest #importing the unittest module
 from user import User #importing the user classs
-
+import pyperclip
 class TestUser(unittest.TestCase):
 
     '''
@@ -14,7 +14,7 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = User("briankc","brian2022",) # create contact object
+        self.new_user = User("briankc","brian2022") # create contact object
 
 
     def test_init(self):
@@ -24,6 +24,7 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(self.new_user.user_name,"briankc")
         self.assertEqual(self.new_user.pass_word,"brian2022")
+        
 
     def test_save_user(self):
         '''
@@ -32,34 +33,92 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user() #saving the user
         self.assertEqual(len(User.user_list),1)  
 
+    
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Collins","Collins20") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
     def tearDown(self):
         '''
         tearDown method that does clean up after each test case has run.
         '''
         User.user_list = []
-   
+
 
     def test_save_multiple_user(self):
         '''
-         test_save_multiple_user to check if we can save multiple user
-            objects to our user_list
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_list
         '''
         self.new_user.save_user()
-        test_user =User("tenda","tenda123")
+        test_user = User("Collins","Collins20") # new user
         test_user.save_user()
-        self.assertEqual(len(User.user_list),2)
+        self.assertEqual(len(User.user_list),2)   
 
     def test_delete_user(self):
         '''
-        test_delete_user to test if we can remove a contact from our contact list
+        test_delete_user to test if we can remove a contact from our user list
         '''
         self.new_user.save_user()
-        test_user = User("tenda","tenda123") # new contact
-        test_user.save_contact()
+        test_user = User("Collins","Collins20") # new user
+        test_user.save_user()
 
-        self.new_user.delete_user()# Deleting a contact object
+        self.new_user.delete_user()# Deleting a user object
         self.assertEqual(len(User.user_list),1)
 
+
+    def test_find_user_by_user_name(self):
+        '''
+        test to check if we can find a user by username and display information
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Collins","Collins20") # new user
+        test_user.save_user()
+
+        found_user = User.find_by_user_name("Collins")
+
+        self.assertEqual(found_user.user_name,test_user.user_name)   
+
+
+    def test_user_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the user.
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Collins","Collins20") # new user
+        test_user.save_user()
+
+        user_exists = User.user_exist("Collins")
+
+        self.assertTrue(user_exists)
+
+    def test_display_all_users(self):
+        '''
+        method that returns a list of all contacts saved
+        '''
+
+        self.assertEqual(User.display_users(),User.user_list)
+
+
+    def test_copy_pass_word(self):
+        '''
+        Test to confirm that we are copying the password address from a found user
+        '''
+
+        self.new_user.save_user()
+        User.copy_pass_word("briankc")
+
+        self.assertEqual(self.new_user.pass_word,pyperclip.paste())
+    
+    
    
 if __name__ == '__main__':
     unittest.main()
